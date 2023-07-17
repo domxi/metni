4bde3ca74f63de49edb318778b75942f6587a60e
Doxmin <mdaisonu@gmail.com>
2023-07-17 12:38:55 -0500

    initial

---
+++ menti/menti/mentiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mentiApp.swift
+//  menti
+//
+//  Created by Dominik on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct mentiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
